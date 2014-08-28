d7e12044c218399c1c534ce6d6842d689942fb59
BlueSurfRhino <richowens@outlook.com>
2014-08-27 18:13:46 -0700

    Threading implemented on Observable Collections
    Leveraged during initial population of songs.

--- CDUINo2/MainWindow.xaml.cs
+++ CDUINo2/MainWindow.xaml.cs
@@ -1,7 +1,10 @@
-﻿using System.Collections.ObjectModel;
+﻿using System;
+using System.Collections.ObjectModel;
 using System.Linq;
+using System.Threading;
 using System.Windows;
 using System.Windows.Controls;
+using System.Windows.Threading;
 
 namespace CDUINo2
 {
@@ -10,17 +13,23 @@ namespace CDUINo2
     /// </summary>
     public partial class MainWindow : Window
     {
-        public ObservableCollection<RcoSong> SongTitleCollection = new ObservableCollection<RcoSong>();
+        public ThreadSafeObservationCollection<RcoSong> SongTitleCollection = new ThreadSafeObservationCollection<RcoSong>();
         //private static readonly CdInfoDbDataContext _CdCatalog = new CdInfoDbDataContext();
         private static readonly iTunesCatalogDataContext _ITunesCatalog = new iTunesCatalogDataContext();
         public MainWindow()
         {
             InitializeComponent();
-            PopulatedObservableCollection(SongTitleCollection);
+            ThreadData ThreadData = new ThreadData();
+            ThreadData.ITunesDC = _ITunesCatalog;
+            ThreadData.ObservableCollection = SongTitleCollection;
+            Worker workerObject = new Worker();
+            Thread workerThread = new Thread(workerObject.LoadObverablesCollection);
+            workerThread.Start(ThreadData);
+            //PopulatedObservableCollection(SongTitleCollection);
             CdDataGrid.DataContext = SongTitleCollection;
         }
 
-        private void PopulatedObservableCollection(ObservableCollection<RcoSong> songTitleCollection)
+        private void PopulatedObservableCollection(ThreadSafeObservationCollection<RcoSong> songTitleCollection)
         {
             SongTitleCollection.Clear();
             foreach (var song in _ITunesCatalog.Songs)
@@ -96,11 +105,181 @@ namespace CDUINo2
             SongTitleCollection.Clear();
             var songTable = _ITunesCatalog.GetTable<Song>();
             var q =
-                songTable.Where(s => s.Name.Contains(tmpSearch));
-            UpDateObservableCollection(SongTitleCollection, q);
+                songTable.Where(s => s.Name.Contains(tmpSearch)).Take(100);
+           UpDateObservableCollection(SongTitleCollection, q);
         }       
     }
 
+    public class Worker
+    {
+        public void LoadObverablesCollection(object data)
+        {
+            ThreadData TData = (ThreadData) data;
+
+            ObservableCollection<RcoSong> SongTitleCollection = TData.ObservableCollection;
+            iTunesCatalogDataContext iTunesCatalog = TData.ITunesDC;
+
+            SongTitleCollection.Clear();
+            foreach (var song in iTunesCatalog.Songs)
+            {
+                var tmpSong = new RcoSong();
+                tmpSong.Id = song.SongID;
+                tmpSong.Name = song.Name;
+                tmpSong.ArtistId = song.ArtistID;
+                tmpSong.AlbumId = song.AlbumID;
+                tmpSong.Album = iTunesCatalog.Albums.First(id => id.AlbumID == song.AlbumID).Title;
+                tmpSong.Artist = song.Artist.ArtistName;
+                tmpSong.PlayCount = song.PlayCount;
+                tmpSong.TrackLength = song.SongTrackLength;
+                SongTitleCollection.Add(tmpSong);
+            }
+
+        }
+
+    }
+
+    public class ThreadData
+    {
+        public iTunesCatalogDataContext ITunesDC;
+        public ThreadSafeObservationCollection<RcoSong> ObservableCollection;
+    }
+
+    public class ThreadSafeObservationCollection<T> : ObservableCollection<T>
+    {
+        private Dispatcher _dispatcher;
+        private ReaderWriterLockSlim _lock;
+
+        public ThreadSafeObservationCollection()
+        {
+            _dispatcher = Dispatcher.CurrentDispatcher;
+            _lock = new ReaderWriterLockSlim();
+
+        }
+
+        protected override void ClearItems()
+        {
+            _dispatcher.InvokeIfRequired(() =>
+            {
+                _lock.EnterWriteLock();
+                try
+                {
+                    base.ClearItems();
+                }
+                finally
+                {
+                    _lock.ExitWriteLock();
+                }
+            }, DispatcherPriority.DataBind);
+        }
+
+        protected override void InsertItem(int index, T item)
+        {
+            _dispatcher.InvokeIfRequired(() =>
+            {
+                if(index >  this.Count)
+                    return;
+                _lock.EnterWriteLock();
+                try
+                {
+                    base.InsertItem(index, item);
+                }
+                finally
+                {
+                    _lock.ExitWriteLock();
+                }
+            }, DispatcherPriority.DataBind);
+        }
+
+        protected override void MoveItem(int oldIndex, int newIndex)
+        {
+            _dispatcher.InvokeIfRequired(() =>
+            {
+
+                _lock.EnterReadLock();
+                Int32 itemCount = this.Count;
+                _lock.ExitReadLock();
+                if(oldIndex >= itemCount | newIndex >= itemCount | oldIndex == newIndex)
+                    return;
+                _lock.EnterWriteLock();
+                try
+                {
+                    base.MoveItem(oldIndex, newIndex);
+                }
+                finally
+                {
+                    _lock.ExitWriteLock();
+                }
+            }, DispatcherPriority.DataBind);
+        }
+
+        protected override void RemoveItem(int index)
+        {
+            _dispatcher.InvokeIfRequired(() =>
+            {
+                if (index >= this.Count)
+                    return;
+                _lock.EnterWriteLock();
+                try
+                {
+                    base.RemoveItem(index);
+                }
+                finally
+                {
+                    _lock.ExitWriteLock();
+                }
+            }, DispatcherPriority.DataBind);
+        }
+
+        protected override void SetItem(int index, T item)
+        {
+            _dispatcher.InvokeIfRequired(() =>
+            {
+                _lock.EnterWriteLock();
+                try
+                {
+                    base.SetItem(index, item);
+                }
+                finally
+                {
+                    _lock.ExitWriteLock();
+                }
+            }, DispatcherPriority.DataBind);
+        }
+
+        public T[] ToSyncArray()
+        {
+            _lock.EnterReadLock();
+            try
+            {
+                T[] _sync = new T[this.Count];
+                this.CopyTo(_sync, 0);
+                return _sync;
+            }
+            finally 
+            {
+                
+                _lock.ExitReadLock();
+            }
+        }
+
+    }
+
+    public static class WPFControlThreadingExtensions
+    {
+        public static void InvokeIfRequired(this Dispatcher disp,
+            Action dotIt,
+            DispatcherPriority priority)
+        {
+            if (disp.Thread != Thread.CurrentThread)
+            {
+                disp.Invoke(priority, dotIt);
+            }
+            else
+            {
+                dotIt();
+            }
+        }
+    }
     public class RcoSong
     {
         private string _name;
